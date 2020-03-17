fb4ca454f065bef17cee3f41f76871e57372e355
Dmitry Murzin <diralik@yandex.ru>
2020-03-18 00:35:40 +0300

    Add debug action

---
+++ src/main/kotlin/indent/rainbow/actions/IrDebugAction.kt
@@ -0,0 +1,8 @@
+package indent.rainbow.actions
+
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.project.DumbAwareAction
+
+class IrDebugAction : DumbAwareAction() {
+    override fun actionPerformed(e: AnActionEvent) {}
+}
