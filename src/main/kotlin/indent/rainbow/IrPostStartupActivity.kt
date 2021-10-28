c45b9b6ef0d633884c2246e3b289b21fa665eff7
Dmitry Murzin <diralik@yandex.ru>
2021-10-28 18:31:50 +0300

    Remove deprecated AppLifecycleListener

--- src/main/kotlin/indent/rainbow/IrPostStartupActivity.kt
+++ src/main/kotlin/indent/rainbow/IrPostStartupActivity.kt
@@ -1,10 +1,11 @@
 package indent.rainbow
 
+import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.StartupActivity
 
-class IrPostStartupActivity : StartupActivity {
+class IrPostStartupActivity : StartupActivity, DumbAware {
     override fun runActivity(project: Project) {
-        IrAnnotatorsManager.initAnnotators()
+        IrApplicationService.INSTANCE.init()
     }
 }
